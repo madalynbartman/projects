47f4ddfe1e6fc0aa32b461e67d750c6c10ccbff9
Madalyn <madalynbartman@gmail.com>
2024-02-23 18:56:54 -0700

    Got further in Learning Bash Scripting from LinkedIn Learning

--- bash.sh
+++ bash.sh
@@ -133,3 +133,69 @@ declare -p # lists the variables you've declared in that session
 
 echo $USER
 
+# $((...)) Arithmetic expansion returns the result of mathematical operations. Older representation: $[...]
+
+# ((...)) Arithmetic evaluation performs calculations and changes the value of variables.
+
+echo $((4+4))
+
+echo $((8-5))
+
+echo $((2*3))
+
+echo $((8/4))
+
+echo $(( (3+6) - 5 * (5*2) ))
+
+a=3
+
+((a+=3))
+echo $a
+
+((a++))
+echo $a
+
+((a++))
+echo $a
+
+((a--))
+echo $a
+
+((a+=2))
+echo $a
+
+((a-=3))
+echo $a
+
+((a*=2))
+echo $a
+
+((a/=2))
+echo $a
+
+b=5
+echo $b
+
+b=$b+2
+
+declare -i b=3
+echo $b
+b=$b+4
+echo $b
+
+echo $((1/3))
+
+# bc and awk can handle floats
+
+declare -i c=1
+declare -i d=3
+
+e=$(echo "scale=3; $c/$d" | bc)
+echo $e
+
+echo $RANDOM
+
+echo $(( 1 + RANDOM % 10 ))
+
+echo $(( 1 + RANDOM % 20 ))
+
