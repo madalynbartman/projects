94a32ad322ed24145c2d1257fd134019ed413d86
Madalyn <madalynbartman@gmail.com>
2024-03-13 05:13:18 -0600

    rps & cysa

---
+++ javascript/rockPaperScissors.js
@@ -0,0 +1,44 @@
+const prompt = require("prompt-sync")();
+
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
+while (true) {
+    const playerChoice = prompt("Enter rock, paper, or scissors: (q to quit): ");
+    if (playerChoice.toLowerCase === "q"
+    ) { 
+        break;
+    }
+    if (
+        playerChoice === "rock" || 
+        playerChoice === "paper" || 
+        playerChoice === "scissors"
+    ) {
+        console.log("PLease enter a valid choice.");
+        continue;
+    }
+
+    // generate a random index that corresponds to 0,1,2 that corresponds to rock, paper, scissors
+    const playerChoices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.round(Math.random() * 2);
+    const computerplayerChoice = playerChoices[randomIndex];
+    console.log(computerplayerChoice);
+
+    if (computerplayerChoice === playerChoice) {
+        console.log("Draw!");
+        ties++;
+    } else if (
+        (playerChoice === "paper" && computerChoice === "rock") || 
+        (playerChoice === "rock" && computerChoice === "scissors") || 
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log("Won!");
+        wins++;
+    } else {
+        console.log("Lost!");
+        losses++;
+    }
+}
+
+console.log("Wins: ", wins, "Losses: ", losses, "Ties: ", ties);
