17968697e612058c8a8b04a4c7238c0760451021
Madalyn <madalynbartman@gmail.com>
2024-03-16 15:27:11 -0600

    File back up program

---
+++ file_backup.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+import datetime
+import schedule
+import time
+
+source_dir = "C:/workspace"
+destination_dir = "C:/workspace/backups"
+
+def copy_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest, str(today))
+    
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to: {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists in: {dest}")
+
+schedule.every().day.at("18:55").do(lambda: copy_folder_to_directory(source_dir, destination_dir))
+
+while True: 
+    schedule.run_pending()
+    time.sleep(60)
+
