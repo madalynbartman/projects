6186d23978db247595a161cae5f5f5de110831d4
Madalyn <madalynbartman@gmail.com>
2024-03-26 23:09:53 -0600

    socketing

--- server.py
+++ server.py
@@ -17,12 +17,14 @@ def handle_client(conn, addr):
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(FORMAT)
-        if msg == DISCONNECT_MESSAGE:
-            connected = False
+        if msg_length: # if not none
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
 
         print(f"[{addr}] {msg}")
+        conn.send("Msg received".encode(FORMAT))
 
     conn.close()
 
