9ff331ab5641afe8829a94b9d061edf78aa01207
madalynbartman <madalynbartman@gmail.com>
2024-03-28 12:22:12 -0600

    space invaders

--- space_invaders.py
+++ space_invaders.py
@@ -10,7 +10,7 @@ pygame.display.set_caption("Space Invaders")
 
 # Load images
 RED_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
-GREEN_SPACESHIPr = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+GREEN_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 BLUE_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
 # Player ship
 YELLOW_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
@@ -51,18 +51,40 @@ class Player(Ship):
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
 
+class EnemyShip(Ship):
+    COLOR_MAP = {
+                "red": (RED_SPACESHIP, RED_LASER),
+                "green": (GREEN_SPACESHIP, GREEN_LASER),
+                "blue": (BLUE_SPACESHIP, BLUE_LASER)
+    }
+    def __init__(self, x, y, color, health=100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def move(self, vel):
+        self.y += vel
+
 def main():
     run = True 
     FPS = 60
-    level = 1
+    level = 0
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
+    lost_font = pygame.font.SysFont("comicsans", 60)
+
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
 
     player_vel = 5
 
     player = Player(300, 650)
 
     clock = pygame.time.Clock()
+
+    lost = False
+    lost_count = 0
 
     def redraw_window():
         # 0,0 is the top left in pygame so y=0 is the very top of the screen
@@ -74,13 +96,37 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label,(WIDTH - level_label.get_width() - 10, 10))
 
-        player.draw(WIN)
+        for enemy in enemies:
+            enemy.draw(WIN) # no draw method in enemies. It inherits it from ship.
+
+        player.draw(WIN) # If overlapping, player will be on top of enemy cause it's drawn second
+
+        if lost:
+            lost_label = lost_font.render("You lost!!", 1, (255,255,255))
+            WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 350))
 
         pygame.display.update()
 
     while run:
         clock.tick(FPS)
         redraw_window()
+        
+        if lives <= 0 or player.health <= 0 : 
+            lost = True
+            lost_count += 1
+
+        if lost:
+            if lost_count > FPS * 3:
+                run = False
+            else:
+                continue
+
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH 100), random.randrange(-1500, 100), random.choice(["red", "blue", "green"]))
+                enemies.appen(enemy)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -98,6 +144,12 @@ def main():
             player.y == player_vel
         if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT: # down
             player.y += player_vel
+
+        for enemy in enemies[:]:
+            enemy.move(enemy_vel)
+            if enemy.y + enemy.get_height() > HEIGHT:
+                lives -= 1
+                enemy.remove(enemy)
 
 main()
 
