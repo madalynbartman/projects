ef8172fb622b84bdd0756751aff39e086b9fdf53
madalynbartman <madalynbartman@gmail.com>
2024-04-01 13:30:42 -0600

    Started Scientific Computing with Python Certification course from Free Code Camp

---
+++ SCWP/chapter1.py
@@ -0,0 +1,38 @@
+number = 5
+text = 'Hello World'
+
+text = 'Hello World'
+print()
+
+text = 'Hello World'
+print(text)
+
+text = 'Hello World'
+print(text[6])
+
+text = 'Hello World'
+print(text[-1])
+
+text = 'Hello World'
+print(len(text))
+
+text = 'Hello World'
+print(type(text))
+
+text = 'Hello World'
+print(type(text))
+shift = 3
+
+text = 'Hello World'
+print(type(text))
+shift = 3
+print(shift)
+
+text = 'Hello World'
+print(type(text))
+shift = 3
+print(type(shift))
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
