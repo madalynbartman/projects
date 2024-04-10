f760ad57acbba1b1e8fe2bf38c01aa27c2539e1d
Madalyn Bartman <madalynbartman@gmail.com>
2024-04-09 18:49:30 -0600

    Data analysis

--- elk/data_analysis.sql
+++ elk/data_analysis.sql
@@ -4,3 +4,15 @@ GET 1/_search
 {
     "track_total_hits": true
 }
+
+GET 1/_search
+{
+    "query": {
+        "range": {
+            "date": {
+                "gte": "2015-06-20",
+                "lte": "2015-09-22"
+            }
+        }
+    }
+}
