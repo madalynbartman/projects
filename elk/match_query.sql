53c7bb7e5d6e9d94fee3c79512a57659486af13c
Madalyn Bartman <madalynbartman@gmail.com>
2024-04-09 20:24:07 -0600

    match phrase query

--- elk/match_query.sql
+++ elk/match_query.sql
@@ -1,7 +1,19 @@
+--match query
 GET 1/_search
 {
     "query":{
         "match":{
+            "headline":{
+                "query": "Shape of you"
+            }
+        }
+    }
+}
+--match phrase query
+GET 1/_search
+{
+    "query":{
+        "match_phrase":{
             "headline":{
                 "query": "Shape of you"
             }
