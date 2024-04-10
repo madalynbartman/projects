7bd09316f967912a3b4b5ebc5986448642110f9a
Madalyn Bartman <madalynbartman@gmail.com>
2024-04-09 21:10:32 -0600

    bool query

---
+++ elk/bool_query.sql
@@ -0,0 +1,21 @@
+-- the bool query has must and must not clauses
+-- can also specify should and filter
+-- should == nice to have
+-- filter == would they fit into a yes or no category. ex: applicant is either in or not in the city with the job
+-- all of these are optional
+GET 1/_search
+{
+    "query": {
+        "match_phrase": {
+            "headline": "Michelle Obama"
+        }
+    },
+    "aggregations": {
+        "category_mentions":{
+            "terms": {
+                "field": "category",
+                "size": 100
+            }
+        }
+    }
+}
