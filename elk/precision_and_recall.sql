bfabc873c41010d8540ab67a0eadadf4391ff9d7
Madalyn Bartman <madalynbartman@gmail.com>
2024-04-09 19:09:05 -0600

    Precision and recall

--- elk/precision_and_recall.sql
+++ elk/precision_and_recall.sql
@@ -8,7 +8,7 @@ GET 1/_search
         }
     }
 }
-
+-- the and operator strings together multiple search terms for better results
 GET news_headlines/_search
 {
     "query": {
@@ -16,6 +16,18 @@ GET news_headlines/_search
             "headline": {
                 "query": "Khloe Kardashian Kendall Jenner",
                 "operator": "and"
+            }
+        }
+    }
+}
+-- for all the hits, at least 3 search terms need to be included in the headline
+GET news_headlines/_search
+{
+    "query": {
+        "match": {
+            "headline": {
+                "query": "Khloe Kardashian Kendall Jenner",
+                "minimum_should_match": 3
             }
         }
     }
