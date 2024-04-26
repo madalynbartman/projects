9dd1b3a65aa4a0aaf4e7987c109e9f353eece3de
Madalyn Bartman <madalynbartman@gmail.com>
2024-04-26 05:11:07 -0600

    mapping

--- elk/mapping.sql
+++ elk/mapping.sql
@@ -19,3 +19,21 @@ POST temp_index/_doc
 }
 -- view the mapping
 GET temp_index/_mapping
+
+POST test_index/_doc
+{
+  "name": "Pineapple",
+  "botanical_name": "Ananas comosus",
+  "produce_type": "Fruit",
+  "country_of_origin": "New Zealand",
+  "date_purchased": "2020-06-02T12:15:35",
+  "quantity": 200,
+  "unit_price": 3.11,
+  "description": "a large juicy tropical fruit consisting of aromatic edible yellow flesh surrounded by a tough segmented skin and topped with a tuft of stiff leaves.These pineapples are sourced from New Zealand.",
+  "vendor_details": {
+    "vendor": "Tropical Fruit Growers of New Zealand",
+    "main_contact": "Hugh Rose",
+    "vendor_location": "Whangarei, New Zealand",
+    "preferred_vendor": true
+  }
+}
