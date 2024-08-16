d3e50ed0b2274c96ba2b425e988c63ff013539e3
Madalyn Bartman <105222619+madalynbartman@users.noreply.github.com>
2024-08-16 12:26:20 -0600

    Create pyfp.py

---
+++ PYFP/pyfp.py
@@ -0,0 +1,32 @@
+# Data Types
+# Int
+76373
+
+# Float
+272.0
+
+# String
+'hello'  
+"hello"
+
+# Bool
+True 
+False
+
+# print
+print('Hello World!')
+print("Hello", 'end', 87, False, end='|') # end is \n by default
+print("Hello")
+
+# variables
+hello = 'waddup'
+world = 'homies'
+print(hello)
+print(hello, world)
+# snake case for variables, camelCase for methods, CapCase for classes
+# you can have caps in your variable names if you want 
+
+# input method 
+# needs a sting
+name = input('Name: ') # include a space so the user doesnt start typing right on the prompt
+print
