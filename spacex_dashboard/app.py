01eaf0590a5d01b3acb735e281335ed9f380250c
Madalyn Bartman <madalynbartman@gmail.com>
2024-08-17 17:55:30 -0600

    SpaceX dashboard

--- spacex_dashboard/app.py
+++ spacex_dashboard/app.py
@@ -3,3 +3,10 @@ from datetime import datetime
 import requests
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hellow world!"
+
+if __name__=="__main__": #ensures you ran this python file directly, not if its used by a diff python file
+    app.run(debug=True)
