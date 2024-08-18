d6f3a685d7230307349eaefcce51a237476bd073
Madalyn Bartman <madalynbartman@gmail.com>
2024-08-17 18:42:14 -0600

    SpaceX dashboard

--- spacex_dashboard/app.py
+++ spacex_dashboard/app.py
@@ -6,7 +6,28 @@ app = Flask(__name__)
 
 @app.route("/") # endpoint
 def index():
-    return "Hello world!"
+    return render_template("index.html", launches=launches)
+
+def fetch_spacex_launches():
+    url = "https://api.spacexdata.com/v4/launches" # get the launch data
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return []
+    
+def categorize_launches(launches): # replaced x with launch
+    sucessful = list(filter(lambda x: x["success"] and not x["upcoming"], launches))
+    failed = list(filter(lambda x: not x["success"] and not x["upcoming"], launches))
+    upcoming = list(filter(lambda x: x["upcoming"], launches))
+
+    return {
+        "sucessful": sucessful,
+        "failed": failed,
+        "upcoming": upcoming
+    }
+
+launches = categorize_launches(fetch_spacex_launches())
 
 if __name__=="__main__": # ensures you ran this python file directly, not if its used by a diff python file
     app.run(debug=True)
